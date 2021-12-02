2422ae5861b5b1eb92354b5a4a700a3c1f273f87
Anders Holst <anders.holst@ri.se>
2021-12-03 00:30:05 +0100

    Dec 3 daily effect

---
+++ xled_plus/samples/day3.py
@@ -0,0 +1,12 @@
+"""
+Day 3 - Gold
+
+Similar to the Sparkles effect in the app, of brigh flashing leds
+against a solid background. This example is meant to mimic the
+"metallic luster" effects of AWW leds, i.e glittering gold.
+"""
+
+from xled_plus.samples.sample_setup import *
+
+ctr = setup_control()
+Gold(ctr).launch_movie()
