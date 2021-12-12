6efae41191261b524240a5a9a10d3f499c1cbc92
Anders Holst <anders.holst@ri.se>
2021-12-12 23:38:00 +0100

    Dec 13 daily effect

---
+++ xled_plus/samples/day13.py
@@ -0,0 +1,11 @@
+"""
+Day 13 - Fire
+
+Saint Lucia day, and the trees are on fire.
+Similar to the Water effect of day 2.
+"""
+
+from xled_plus.samples.sample_setup import *
+
+ctr = setup_control()
+Fire(ctr).launch_movie()
