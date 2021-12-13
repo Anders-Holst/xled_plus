bbfba09a13241120daaab1209f86ac774b775ccb
Anders Holst <anders.holst@ri.se>
2021-12-14 00:42:25 +0100

    Dec 14 daily effect

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '0.1.14'
+__version__ = '0.1.15'
 
 _classifiers = [
     'Development Status :: 4 - Beta',
---
+++ xled_plus/samples/day14.py
@@ -0,0 +1,13 @@
+"""
+Day 14 - Looping lights, red, magenta, green
+
+Another variant of SparkleEffect, this time each led "looping" from black
+through one of the selected colors to white, and then dims back to black
+again (through gray). Gives nice white mix among the pulsating colors.
+"""
+
+from xled_plus.samples.sample_setup import *
+
+ctr = setup_control()
+colfunc = selected_color_func([(0.6, 1.0), (0.7, 1.0), (0.28, 1.0)])
+SparkleEffect(ctr, 3, colfunc, looplight_func(16, 16)).launch_movie()
